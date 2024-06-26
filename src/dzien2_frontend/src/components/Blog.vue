788a7d5dc55560533742380116ce7cc8984bc198
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-06-26 19:17:04 +0200

    Added style for front

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,12 +1,25 @@
 <template>
-    <button @click="getEntries">
-        refresh
-    </button>
-    <div>
-        <p v-for="entry in wpisy">
-            {{ entry }}
-        </p>
+    <h2 class="text-blue-600">Wpisy na bloga:</h2>
+    <div class="w-100 flex flex-row-reverse">
+        <button @click="getEntries" class="bg-blue-600 rounded-md text-white p-4">
+            refresh
+        </button>
     </div>
+    <div class="grid mx-6 gap-4 my-4">
+        <div v-for="entry in wpisy" class="grid drop-shadow-xl bg-stone-300 p-4">
+            <p>
+                {{ entry }}
+            </p>
+        </div>
+    </div>
+    <div class="flex justify-center flex-col">
+        <input type="text" v-model="nowyBlog" class="border-2 border-blue-600">
+        <button @click="addEntry" class="bg-blue-600 rounded-md text-white p-4">
+            dodaj
+        </button>
+    </div>
+
+
 </template>
 
 <script>
